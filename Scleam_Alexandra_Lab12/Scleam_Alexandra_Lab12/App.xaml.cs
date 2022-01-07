cc9ac271cd4b98df76fc026f61c0b7c88c854007
IE2.Scleam. Alexandra <alexandra.scleam@gmail.com>
2022-01-06 16:26:09 -0800

    Add project files.

---
+++ Scleam_Alexandra_Lab12/Scleam_Alexandra_Lab12/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Scleam_Alexandra_Lab12.Services;
+using Scleam_Alexandra_Lab12.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Scleam_Alexandra_Lab12.Data;
+
+
+namespace Scleam_Alexandra_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Scleam_Alexandra_Lab12/Scleam_Alexandra_Lab12/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Scleam_Alexandra_Lab12.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Scleam_Alexandra_Lab12.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
